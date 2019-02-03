9f8a8424680d4c3142aae8b3d4328f078d300347
belmehdi sofiane <belmehdisofiane@gmail.com>
2019-02-03 22:30:57 +0100

    Works but we can do better

--- malloc/src/implementation_malloc.c
+++ malloc/src/implementation_malloc.c
@@ -8,50 +8,80 @@
 #include "malloc_header.h"
 void *base = NULL;
 
-t_block get_first_fit_block(t_block *last, size_t size)
+/**
+ * return the first fit block in memorie
+ * @param last
+ * @param size
+ * @return
+ */
+t_block get_first_fit_block_algo(t_block *last, size_t size)
 {
-	t_block b = base;
-	while (b && !(b->free && b->size >= size )) {
-		*last = b;
-		b = b->next;
+	t_block base_block = base;
+	while (base_block == NULL
+			&& !(base_block->free && base_block->size >= size)) {
+		*last = base_block;
+		base_block = base_block->next;
 	}
-	return (b);
+	return (base_block);
 }
 
-t_block extend_heap (t_block last , size_t s)
+/**
+ * rise the size of the heap
+ * @param last_block
+ * @param size_to_expend
+ * @return
+ */
+t_block heap_extention(t_block last_block, size_t size_to_expend)
 {
-	t_block b;
-	b = sbrk (0);
-	if (sbrk( BLOCK_SIZE + s) == (void*) -1)
+	t_block block = NULL;
+	block = sbrk(0);
+
+	if (sbrk(BLOCK_SIZE + size_to_expend) == (void*)-1) {
 		return (NULL );
-	b->size = s;
-	b->next = NULL;
-	if (last)
+	}
 
-		last ->next = b;
-	b->free = 0;
-	return (b);
+	block->size = size_to_expend;
+	block->next = NULL;
+
+	if (last_block) {
+		last_block->next = block;
+	}
+
+	block->free = 0;
+	return (block);
 }
 
-void split_block(t_block b, size_t s)
+/**
+ * Split a block of memorie block_to_split
+ * to the size of the geven parameter size_of_new_lock
+ * @param block_to_split
+ * @param size_of_new_lock
+ */
+void split_block(t_block block_to_split, size_t size_of_new_lock)
 {
-	t_block new_block;
-	new_block = b->data + s;
-	new_block->size = b->size - s - BLOCK_SIZE;
-	new_block->next = b->next;
+	t_block new_block = NULL;
+	new_block = (t_block)(block_to_split->data + size_of_new_lock);
+	new_block->size = block_to_split->size - size_of_new_lock - BLOCK_SIZE;
+	new_block->next = block_to_split->next;
 	new_block->free = 1;
-	b->size = s;
-	b->next = new_block;
+	block_to_split->size = size_of_new_lock;
+	block_to_split->next = new_block;
 }
-void *malloc(size_t size)
+
+/**
+ * malloc implementation
+ * @param size
+ */
+void *my_malloc(size_t size)
 {
-	t_block new_block, last;
+	t_block new_block = NULL;
+	t_block last = NULL;
 	size_t s;
 	s = align4(size);
 	if (base) {
 		/* First find a block */
 		last = base;
-		new_block = get_first_fit_block(&last, s);
+		new_block = get_first_fit_block_algo(&last, s);
 		if (new_block) {
 			/* can we split */
 			if ((new_block->size - s) >= ( BLOCK_SIZE + 4)) {
@@ -60,16 +90,23 @@ void *malloc(size_t size)
 			new_block->free = 0;
 		} else {
 			/* No fitting block , extend the heap */
-			new_block = extend_heap(last, s);
+			new_block = heap_extention(last, s);
 			if (!new_block)
 				return (NULL);
 		}
 	} else {
 		/* first time */
-		new_block = extend_heap(NULL, s);
+		new_block = heap_extention(NULL, s);
 		if (!new_block)
 			return (NULL);
 		base = new_block;
 	}
 	return (new_block->data);
 }
+
+int main(int argc, char **argv)
+{
+	my_malloc(argc);
+	return 0;
+}
+
--- malloc/src/malloc_header.h
+++ malloc/src/malloc_header.h
@@ -6,20 +6,22 @@
 
 #ifndef MALLOC_HEADER_H_
 #define MALLOC_HEADER_H_
-#define align4(x) (((((x) - 1) >> 2) << 2) + 4)
-#define BLOCK_SIZE sizeof(struct s_block)
 typedef struct s_block *t_block;
 
 struct s_block {
 	// the size of the data
 	size_t size;
+	//the previous block
+	t_block prev;
 	// the next block
 	t_block next;
 	// a flag to know if the current block is free or not
 	int free;
+	// the pointer of the array indicate the end of meta data
 	char data[1];
 };
 
-
+#define align4(x) (((((x) - 1) >> 2) << 2) + 4)
+#define BLOCK_SIZE 12
 
 #endif /* MALLOC_HEADER_H_ */
